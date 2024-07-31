5b9de07a141fceeabe00f02f9e734938182c6df4
thanh.duong <106790691+thanh-duong-iij@users.noreply.github.com>
2024-07-31 11:37:33 +0700

    Update install-requirements.sh

--- infrastructure/requirements/install-requirements.sh
+++ infrastructure/requirements/install-requirements.sh
@@ -2,6 +2,16 @@
 
 set -x
 
+curl -fsSL "https://releases.hashicorp.com/terraform/1.9.3/terraform_1.9.3_linux_amd64.zip" \
+    -o /tmp/terraform_linux_amd64.zip && \
+    unzip "/tmp/terraform_linux_amd64.zip" -d /usr/bin/ && \
+    rm /tmp/terraform_linux_amd64.zip
+
+curl -fsSL "https://releases.hashicorp.com/packer/1.11.1/packer_1.11.1_linux_amd64.zip" \
+    -o /tmp/packer_linux_amd64.zip && \
+    unzip "/tmp/packer_linux_amd64.zip" -d /usr/bin/ && \
+    rm /tmp/packer_linux_amd64.zip
+
 apt-get update
 sed 's/#.*//' requirements.apt | xargs apt-get install
 apt-get clean all
